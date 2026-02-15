0e043b26d6753504c69a8be2661e3a54192852c4
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-02-15 23:13:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Docker container is working!"
+    return "Auth Service Running"
+
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
